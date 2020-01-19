100106680399257d8bd3d96c1b74689715b7714d
Matt Milton <mattmilton57@gmail.com>
2020-01-19 22:28:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -68,7 +68,7 @@ end
 
 def checkout(cart, coupons)
   consolodated_cart = consolidate_cart(cart)
-  couponed_cart = apply_coupons(consolodated_cart)
+  couponed_cart = apply_coupons(consolodated_cart, coupons)
   final_cart = apply_clearance(couponed_cart)
   
   total = 0 
